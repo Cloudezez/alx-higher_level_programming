0553ba20c67af984aacae4ffe8f0f1f1bec4df2f
cloudezez <cloudezez@gmail.com>
2024-08-07 03:07:52 -0700

    m

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request to that URL, and displays the size of the body of the response in bytes
-
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
+# Sends a request to the URL and displays the size of the body of the response
 curl -s "$1" | wc -c
-
