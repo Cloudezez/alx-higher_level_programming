dac3fd11c3ca087ec1527773435828941dbd5aa8
cloudezez <cloudezez@gmail.com>
2024-08-07 02:58:27 -0700

    m

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Displays the body of a 200 status code response from a URL
-curl -sL "$1"
-
+# Sends a GET request to the URL and displays the body of a 200 status code response
+if [ "$(curl -sL -w "%{http_code}" -o /dev/null "$1")" -eq 200 ]; then curl -sL "$1"; fi
