4cc9a4ccbd17acad3c82b02c4424b0efab97a70b
cloudezez <cloudezez@gmail.com>
2024-08-07 04:29:13 -0700

    m

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from urllib import request
+
+url = "https://alx-intranet.hbtn.io/status"
+with request.urlopen(url) as response:
+    body = response.read()
+    print("Body response:")
+    print(f"    - type: {type(body)}")
+    print(f"    - content: {body}")
+    print(f"    - utf8 content: {body.decode('utf-8')}")
