ba4cec570ea90dc2cbd6c0f47e1b62458138caa4
cloudezez <cloudezez@gmail.com>
2024-08-18 08:48:00 -0700

    m

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
+"""A script that:
+- takes your GitHub credentials (username and password)
+- uses the GitHub API to display your id
+"""
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
+
 if __name__ == "__main__":
-    # Retrieve command-line arguments
-    username = sys.argv[1]
-    token = sys.argv[2]
-    
-    # Print to verify the arguments are correct
-    print(f"Username: {username}")
-    print(f"Token: {token}")
-
-    # Define the URL for the GitHub API
-    url = "https://api.github.com/user"
-    
-    # Make the GET request with Basic Authentication
-    response = requests.get(url, auth=HTTPBasicAuth(username, token))
-    
-    # Print the JSON response and status code
-    print(response.json())
-    print(response.status_code)
-
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
+"""A script that:
+- takes your GitHub credentials (username and password)
+- uses the GitHub API to display your id
+"""
 import sys
 import requests
+from requests.auth import HTTPBasicAuth
 
-def fetch_commits(repo_name, owner_name):
-    """Fetch and print the 10 most recent commits for a given repository."""
-    url = f"https://api.github.com/repos/Cloudezez/alx-higher_level_programming/commits"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        commits = response.json()
-        for commit in commits[:10]:  # Get the most recent 10 commits
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
-            print(f"{sha}: {author_name}")
-    else:
-        print("Error: Unable to fetch commits")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./100-github_commits.py alx-higher_level_programming Cloudezez")
-    else:
-        repo_name = sys.argv[1]
-        owner_name = sys.argv[2]
-        fetch_commits(repo_name, owner_name)
-
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
